422651550f48161d8d1d4c28fd5d28d11d1d0165
Chitesh Tewani <chiteshtewani@gmail.com>
2016-12-02 19:58:49 -0500

    mean and variance of FG and BG

--- data.py
+++ data.py
@@ -67,6 +67,47 @@ def create_graph(image, image_seed):
                         adjNode.setTop(edge)
     return nodeMap
 
+def compute_mean_variance(image, image_seed):
+    image = np.array(image)
+    image_seed = np.array(image_seed)
+
+    nrow, ncol = image.shape
+    print nrow, ncol
+
+    count = [0.0 for _ in xrange(constants.CLASSIFICATION_TYPE)]
+
+    mean = [[0.0 for _ in xrange(constants.CLASSIFICATION_TYPE)] for _ in xrange(constants.NUMBER_OF_COLORS)]
+    var = [[0.0 for _ in xrange(constants.CLASSIFICATION_TYPE)] for _ in xrange(constants.NUMBER_OF_COLORS)]
+    # iterate over each node of image
+    for rowIndex in xrange(nrow):
+        for colIndex in xrange(ncol):
+            currPosition = (rowIndex, colIndex)
+            rgb = image_seed[currPosition]
+            r, g, b = rgb
+            label = None
+
+            # set to blue --> foreground
+            if max(r, g) < 100 and b > 100:
+                count[constants.FOREGROUND] += 1
+                label = constants.FOREGROUND
+
+            # set to blue --> background
+            if max(g, b) < 100 and r > 100:
+                count[constants.BACKGROUND] += 1
+                label = constants.BACKGROUND
+
+            for colorIndex in xrange(constants.NUMBER_OF_COLORS):
+                mean[label][colorIndex] += rgb[colorIndex]
+                var[label][colorIndex] = pow(rgb[colorIndex], 2)
+
+    for colorIndex in xrange(constants.NUMBER_OF_COLORS):
+        for labelIndex in xrange(constants.CLASSIFICATION_TYPE):
+            mean[labelIndex][colorIndex] /= count[labelIndex]
+            var[labelIndex][colorIndex] = var[labelIndex][colorIndex]/count[labelIndex] - \
+                pow(mean[labelIndex][colorIndex], 2)
+
+    return mean, var
+
 def main():
     nara = read_image('dataset/nara.png')
     nara_seed = read_image_rgb('dataset/nara-seeds.png')
